3d25cb3ae8c81d676ff53610cf40240cc96219df
JaySant <jaysant15@gmail.com>
2025-07-01 19:25:20 -0300

    feat: implementa desafio Sintaxe: realiza cadastro da conta do banco via terminal

--- ContaBanco/ContaTerminal.java
+++ ContaBanco/ContaTerminal.java
@@ -1,4 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Por favor, digite o numero da Conta: ");
+        int numeroConta = input.nextInt();
+
+        System.out.print("Por favor, digite o numero da Agência: ");
+        String numeroAgencia = input.next();
+
+        System.out.print("Por favor, digite o seu Nome completo: ");
+        input.nextLine();
+        String nomeCliente = input.nextLine();
+
+        System.out.print("Por favor, insira o seu saldo: ");
+        float saldoCliente = input.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, " +
+                        "conta %d e seu saldo %.2f já está disponível para saque", nomeCliente, numeroAgencia, numeroConta, saldoCliente);
+        input.close();
     }
 }
